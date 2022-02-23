d70124e62cef104fcee30b98b3da1f81386574af
Shoham Elias <shohamelias@gmail.com>
2022-02-23 22:28:40 +0200

    First Commit

---
+++ src/primitives/Point.java
@@ -0,0 +1,21 @@
+package primitives;
+
+public class Point 
+{
+	Double3 xyz;
+	
+	public Point(double x,double y,double z) 
+	{
+		xyz=new Double3(x, y, z);
+	}
+	public Point(Double3 d)
+	{
+		xyz=new Double3(d.d1,d.d2,d.d3);
+	}
+	
+	public Vector Substruct(Point p) 
+	{
+		
+	}
+
+}

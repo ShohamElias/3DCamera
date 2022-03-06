fa70d6a6a05130e5cf5abeb9137e30bca785139c
Shoham Elias <shohamelias@gmail.com>
2022-03-06 20:44:32 +0200

    תקפצו לי

---
+++ src/unittest/SphereTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shoham
+ *
+ */
+class SphereTests {
+
+	/**
+	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
